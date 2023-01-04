0c845c9241b7097ffda6dca74dcfbb5c297fd397
IgorNunesFontoura <54515475+Igor-Nunes-Fontoura@users.noreply.github.com>
2023-01-04 01:59:02 +0000

    Python

---
+++ Exercicios_Python/aula003.py
@@ -0,0 +1,14 @@
+# String = texto
+
+# Aspas duplas
+print("Igor Nunes")
+
+# Aspas simples
+print('Igor Nunes')
+
+# Escape
+print("Igor \"Vegetta\" Nunes")
+
+# Macete
+print('Igor "Vegetta" Nunes')
+print("Igor 'Vegetta' Nunes")
---
+++ Exercicios_Python/aula004.py
@@ -0,0 +1,15 @@
+# int e float = números
+# int = número inteiro
+print(1)
+print(0)
+print(-1)
+
+# float = número com ponto flutuante
+print(1.0)
+print(0.0)
+print(-1.0)
+
+# Função type
+print(type("s"))
+print(type(0))
+print(type(0.0))
